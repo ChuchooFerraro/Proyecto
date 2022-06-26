e22cb069ed11f77d16232c03cbb4f5a1517cd508
ChuchooFerraro <guidoaferraro@gmail.com>
2022-06-26 15:02:20 -0300

    Cambie nombres de carpetas

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from home import views
-
-app_name='home'
-urlpatterns = [
-    path('', views.index, name='main'),
-    path('search', views.search, name='search'),
-]
